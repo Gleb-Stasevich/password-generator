8ee5486b1ba8d9a5e13fc9b8de73dfe531b7d15b
Gleb-Stasevich <gfox.12@mail.ru>
2023-04-05 22:24:54 +0300

    generate project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--template-->
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Barlow&family=Tomorrow:wght@400;600;700&display=swap");
+
+$orange: #f8ef00;
+$light: #fafafa;
+$aqua: #00f0ff;
+$grey: #bbbbbb;
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: black;
+}
+
+li,
+a {
+  list-style: none;
+  text-decoration: none;
+}
+</style>
---
+++ src/components/passwordHistory.vue
@@ -0,0 +1,26 @@
+/*eslint-disable*/
+<template>
+    <!--template-->
+</template>
+  
+<script>
+
+export default {
+
+}
+</script>
+  
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Barlow&family=Tomorrow:wght@400;600;700&display=swap");
+
+$orange: #f8ef00;
+$light: #fafafa;
+$aqua: #00f0ff;
+$grey: #bbbbbb;
+
+* {
+    font-family: "Barlow", sans-serif;
+    font-family: "Tomorrow", sans-serif;
+}
+</style>
+  
