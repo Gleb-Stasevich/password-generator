94f50f361e94ee43c767985dc35c13a69c404ee3
Gleb-Stasevich <gfox.12@mail.ru>
2023-04-09 21:01:43 +0300

    build project

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex';
-
-import { passwordModule } from '@/store/passwordModule.js';
-
-export default createStore({
-    modules: {
-        password: passwordModule
-    }
-})
